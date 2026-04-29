de46a8ae66695448bdb3ff35e27a9eff24e27a4c
alunosenai <joao.p.souza9@ba.estudante.senai.br>
2026-04-28 21:11:17 -0300

    aprendendo agenda facil

---
+++ agendafacil/src/Pages/PaginaPrincipal/index.jsx
@@ -0,0 +1,12 @@
+import '/.style.css';
+
+
+export default function PaginaPrincipal(){
+    return(
+        <div className="PaginaPrincipal">
+            <section className="hero">
+                
+            </section>
+        </div>
+    )
+}
